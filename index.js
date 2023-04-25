a72bb6597342e262e7147f784504933a72a6aa69
shojol021 <shojol021@gmail.com>
2023-04-26 02:12:04 +0600

    dynamic news  url

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 
 const data = require('./data.json')
+const news = require('./news.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World 2')
@@ -15,6 +16,28 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(data)
 })
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '0') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const singleNews = news.find(n => n._id === id)
+    res.send(singleNews)
+})
+
 
 app.listen(port, () => {
     console.log('hello from port ', port)
